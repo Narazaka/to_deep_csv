c692f7ceab9dc4780a1ca783c090fda25bd199bb
Narazaka <info@narazaka.net>
2020-03-06 07:24:50 +0900

    subject

--- test/test_basic.rb
+++ test/test_basic.rb
@@ -1,36 +1,56 @@
 require_relative "./test_helper"
 
 describe Array do
+  subject { values.to_deep_csv }
+
   describe "empty" do
+    let(:values) { [] }
     it "should be empty value" do
-      assert { [].to_deep_csv == "" }
+      assert { subject == "" }
     end
   end
 
   describe "array of array" do
-    it "numbers" do
-      assert { [[1, 2, 3.1]].to_deep_csv == "1,2,3.1\n" }
+    describe "numbers" do
+      let(:values) { [[1, 2, 3.1]] }
+      it "should csv" do
+        assert { subject == "1,2,3.1\n" }
+      end
     end
-    it "strings" do
-      assert { [%w[aa cc]].to_deep_csv == "aa,cc\n" }
+    describe "strings" do
+      let(:values) { [%w[aa cc]] }
+      it "should csv" do
+        assert { subject == "aa,cc\n" }
+      end
     end
-    it "commas" do
-      assert { [%w[a,a cc]].to_deep_csv == "\"a,a\",cc\n" }
+    describe "commas" do
+      let(:values) { [%w[a,a cc]] }
+      it "should csv" do
+        assert { subject == "\"a,a\",cc\n" }
+      end
     end
-    it "headers" do
-      assert { [%w[aa cc]].to_deep_csv(headers: true, columns: %i[a b]) == "a,b\naa,cc\n" }
+    describe "headers" do
+      let(:values) { [%w[aa cc]] }
+      subject { values.to_deep_csv(headers: true, columns: %i[a b]) }
+      it "should csv" do
+        assert { subject == "a,b\naa,cc\n" }
+      end
     end
   end
 
   describe "array of hash" do
-    it "numbers" do
-      assert { [{a: 1, b: 2, c: 3.1}].to_deep_csv == "1,2,3.1\n" }
+    describe "strings" do
+      let(:values) { [{ q: :aa, r: :cc }] }
+      it "should csv" do
+        assert { subject == "aa,cc\n" }
+      end
     end
-    it "strings" do
-      assert { [{q: :aa, r: :cc}].to_deep_csv == "aa,cc\n" }
-    end
-    it "headers" do
-      assert { [{q: :aa, r: :cc}].to_deep_csv(headers: true) == "q,r\naa,cc\n" }
+    describe "headers" do
+      let(:values) { [{ q: :aa, r: :cc }] }
+      subject { values.to_deep_csv(headers: true) }
+      it "should csv" do
+        assert { subject == "q,r\naa,cc\n" }
+      end
     end
   end
 end
